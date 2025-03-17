5a38590130cff17db2e8e784622afa24da7cf941
ashwin8106 <ashwinsiva.678@gmail.com>
2025-03-17 07:33:50 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 //Main class
-
 import src.HomeGUI;
+import src.MainGUI;
+import src.InventoryManager;
 public class Main {
     public static void main(String[] args) {
         // Start the application by creating the HomeGUI
